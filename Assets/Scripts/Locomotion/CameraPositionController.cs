b3839e92012300d2f2cb7c04acef6cc0869a3e42
Artem Konyshev <konyshev_arte@mail.ru>
2019-01-26 00:01:32 +0500

    added mouse click locomotion; added navmeshagent to npc; added camera follow

---
+++ Assets/Scripts/Locomotion/CameraPositionController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositionController : MonoBehaviour
+{
+
+    public Vector3 CameraOffset;
+
+    void Update()
+    {
+        Camera.main.transform.position = transform.position + CameraOffset;
+    }
+
+}
---
+++ Assets/Scripts/Locomotion/WalkController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class WalkController : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
