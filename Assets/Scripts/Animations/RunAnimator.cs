c5821266c642bcbd75553b929afc896733bb4d34
Artem Konyshev <konyshev_arte@mail.ru>
2019-01-26 00:41:48 +0500

    added player avatar run and idle animations

---
+++ Assets/Scripts/Animations/RunAnimator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator))]
+public class RunAnimator : MonoBehaviour
+{
+    public NavMeshAgent NavMeshAgent;
+    public float AnimatorSpeedFactor;
+
+    private Animator animator;
+    private float currentVelocity;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        currentVelocity = Mathf.Lerp(currentVelocity, NavMeshAgent.velocity.normalized.magnitude, Time.deltaTime * AnimatorSpeedFactor);
+        animator.SetFloat("Speed", currentVelocity);
+    }
+
+}
