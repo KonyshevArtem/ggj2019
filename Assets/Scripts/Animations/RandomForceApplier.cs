a22b3a9f493dc33cd23e6a8c8f7671eba2738571
Artem Konyshev <konyshev_arte@mail.ru>
2019-01-25 23:14:13 +0500

    added charater prefab with dance animation

---
+++ Assets/Scripts/Animations/RandomForceApplier.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomForceApplier : MonoBehaviour
+{
+
+    public float MaxForce;
+
+    private Rigidbody rg;
+    private ActionRepeater actionRepeater;
+
+    void Start()
+    {
+        rg = GetComponent<Rigidbody>();
+        actionRepeater = new ActionRepeater(0.5f, ApplyForce);
+    }
+
+    void Update()
+    {
+        actionRepeater.Tick(Time.deltaTime);
+    }
+
+    void ApplyForce()
+    {
+        rg.AddForce(Random.Range(-MaxForce, MaxForce), Random.Range(-MaxForce, MaxForce), Random.Range(-MaxForce, MaxForce), ForceMode.Impulse);
+    }
+
+}
---
+++ Assets/Scripts/Utils/ActionRepeater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JetBrains.Annotations;
+
+public class ActionRepeater
+{
+    private readonly float defaultTime;
+    private readonly Action action;
+
+    private float currentTime;
+
+    public ActionRepeater(float repeatTime, [NotNull] Action action)
+    {
+        defaultTime = repeatTime;
+        currentTime = repeatTime;
+        this.action = action;
+    }
+
+    public void Tick(float deltaTime)
+    {
+        currentTime -= deltaTime;
+        if (currentTime <= 0)
+        {
+            action();
+            currentTime = defaultTime;
+        }
+    }
+}
