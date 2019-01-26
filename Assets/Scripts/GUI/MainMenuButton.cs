77962b618106ba306f66735c8bfcdff44c04b7d7
Rustem Alkapov <ralkapov@gmail.com>
2019-01-27 01:06:09 +0500

    add buttons

---
+++ Assets/Scripts/GUI/MainMenuButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class MainMenuButton : MonoBehaviour
+{
+
+    public GameObject RedLayer;
+    public GameObject BlackLayer;
+    public GameObject BlueLayer;
+    public int ShiftScale = 10;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+	    var direction = Input.mousePosition - BlackLayer.gameObject.transform.position;
+	    var shift = ShiftScale * direction.normalized;
+	    RedLayer.gameObject.transform.position = BlackLayer.gameObject.transform.position - shift;
+	    BlueLayer.gameObject.transform.position = BlackLayer.gameObject.transform.position + shift;
+    }
+}
